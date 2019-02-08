530eba34b97e16bb4bf1aec65269c3e09fe6388d
Pedro Carolina <pedrocarolina@polygons-MacBook-Pro-2.local>
2019-02-08 10:24:12 +0100

    Delete undeleted files

--- config/stripe.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/** @type {import('@adonisjs/framework/src/Env')} */
-const Env = use('Env')
-
-module.exports = {
-  secret: Env.get('STRIPE_API_KEY', 'SECRET'),
-  public: Env.get('STRIPE_PUBLIC_KEY', 'PUBLIC')
-}
--- resources/js/components/index.js
+++
@@ -1,7 +0,0 @@
-import vueSlider from 'vue-slider-component'
-
-export default {
-  install(Vue, options) {
-    Vue.component('VueSlider', vueSlider)
-  }
-}
